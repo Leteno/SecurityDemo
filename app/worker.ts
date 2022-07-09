0cff93dd8549fd92eb914786c45daaede15390b3
Leteno <changchunjunhao@gmail.com>
2022-07-09 08:24:29 +0800

    Try to fix ban-worker-calls issue

--- app/worker.ts
+++ app/worker.ts
@@ -1,4 +1,18 @@
+import {trustedTypes} from 'trusted-types'
 
 console.log("hello world");
 
-new Worker("https://www.baidu.com");
+const workerPolicy = trustedTypes.createPolicy('my-worker-policy', {
+  createScriptURL: url => {
+    if (url.startsWith(globalThis.origin + '/')) {
+      return url;
+    }
+    if (url == "https://www.baidu.com") {
+      return url;
+    }
+
+    throw Error(`[my-worker-policy][createScriptURL] URL is not trusted, it is: ${url}`)
+  }
+});
+let workerSrc = workerPolicy.createScriptURL("https://www.baidu.com");
+new Worker(workerSrc);
