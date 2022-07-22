a148ec9bd439a8c98a6d8930e0b70a94bc8e34b1
Leteno <changchunjunhao@gmail.com>
2022-07-23 00:00:10 +0800

    Evil happen: start dealing with the notification from other app

--- android/secuirtydemo2/app/src/main/java/com/juzhen/secuirtydemo2/sender/CallUnsafeReceiver01.java
+++ android/secuirtydemo2/app/src/main/java/com/juzhen/secuirtydemo2/sender/CallUnsafeReceiver01.java
@@ -8,6 +8,7 @@ public class CallUnsafeReceiver01 {
     public static void call(Context context) {
         Log.d("juzhen", "CallUnsafeReceiver01 call UnsafeReceiver01");
         Intent intent = new Intent();
+        intent.setPackage("com.juzhen.securitydemo");
         intent.setAction("securitydemo.unsafereceiver01");
         context.sendBroadcast(intent);
     }
