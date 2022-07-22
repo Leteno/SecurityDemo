eca5351003ec61c4af3689508f0ef250c9fac927
Leteno <changchunjunhao@gmail.com>
2022-07-22 23:51:37 +0800

    Cannot receive broadcast call: 'BroadcastQueue: Background execution not allowed'

--- android/secuirtydemo2/app/src/main/java/com/juzhen/secuirtydemo2/FirstFragment.kt
+++ android/secuirtydemo2/app/src/main/java/com/juzhen/secuirtydemo2/FirstFragment.kt
@@ -1,12 +1,14 @@
 package com.juzhen.secuirtydemo2
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.juzhen.secuirtydemo2.databinding.FragmentFirstBinding
+import com.juzhen.secuirtydemo2.sender.CallUnsafeReceiver01
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -35,6 +37,14 @@ class FirstFragment : Fragment() {
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+        var handler = Handler()
+        var callInBg: Runnable = object: Runnable {
+            override fun run() {
+                CallUnsafeReceiver01.call(context)
+                handler.postDelayed(this, 10 * 1000);
+            }
+        }
+        handler.post(callInBg);
     }
 
     override fun onDestroyView() {
---
+++ android/secuirtydemo2/app/src/main/java/com/juzhen/secuirtydemo2/sender/CallUnsafeReceiver01.java
@@ -0,0 +1,14 @@
+package com.juzhen.secuirtydemo2.sender;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class CallUnsafeReceiver01 {
+    public static void call(Context context) {
+        Log.d("juzhen", "CallUnsafeReceiver01 call UnsafeReceiver01");
+        Intent intent = new Intent();
+        intent.setAction("securitydemo.unsafereceiver01");
+        context.sendBroadcast(intent);
+    }
+}
